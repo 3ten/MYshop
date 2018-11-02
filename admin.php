15ab8a7e0b6b3f9120b1088ba89122f99f5eaae6
3ten <3ten@users.noreply.github.com>
2018-11-02 15:40:00 +0700

    github test

--- admin.php
+++ admin.php
@@ -26,7 +26,7 @@ if ($_FILES['myfile']['error'] == 0) {
 <head>
     <link rel="stylesheet" href="css/bootstrap/bootstrap.min.css">
     <link rel="stylesheet" href="css/style.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script> -->
     <!-- <script src="js/jquery-3.3.1.min.js"></script> -->
     <script src="js/jquery.min.js"></script>
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
